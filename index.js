307bf6c701f7ef472262ee222733dabddc91dd5e
Scot Pfleghaar <scotpfleghaar@Scots-iMac.local>
2021-08-03 15:39:16 -0500

    update

--- index.js
+++ index.js
@@ -3,18 +3,43 @@ const app = express()
 const port = 3000
 var os = require('os-utils');
 
+const iterations = 50;
+const multiplier = 1000000000;
+
+function calculatePrimes(iterations, multiplier) {
+  var primes = [];
+  for (var i = 0; i < iterations; i++) {
+    var candidate = i * (multiplier * Math.random());
+    var isPrime = true;
+    for (var c = 2; c <= Math.sqrt(candidate); ++c) {
+      if (candidate % c === 0) {
+          // not prime
+          isPrime = false;
+          break;
+       }
+    }
+    if (isPrime) {
+      primes.push(candidate);
+    }
+  }
+  return primes;
+}
+
+function doPointlessComputationsWithBlocking() {
+  var primes = calculatePrimes(iterations, multiplier);
+}
 
 
 app.get('/', (req, res) => {
     os.cpuUsage(function(v){
+        console.log('CPU Usage (%): ' + v)
+    });
     if (Math.floor(Math.random() * 3) > 1) {
-        setTimeout(() => {
-                res.send('CPU Usage Inside (%): ' + v)
-        }, 60000)
+        doPointlessComputationsWithBlocking()
+        res.send('timeOut')
     } else {
-        res.send('CPU Usage (%): ' + v)
+        res.send('hello')
     }
-});
 })
 
 app.listen(port, () => {
