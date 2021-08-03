fd81318d3275b59d212087f6e9a264bd8e99ff36
Scot Pfleghaar <scotpfleghaar@Scots-iMac.local>
2021-08-03 15:13:36 -0500

    update

--- index.js
+++ index.js
@@ -2,11 +2,25 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+var fibonacci_series = function (n) 
+{
+  if (n===1) 
+  {
+    return [0, 1];
+  } 
+  else 
+  {
+    var s = fibonacci_series(n - 1);
+    s.push(s[s.length - 1] + s[s.length - 2]);
+    return s;
+  }
+};
+
 app.get('/', (req, res) => {
     if (Math.floor(Math.random() * 3) > 1) {
-        setTimeout(function(){ 
-            res.send('Hello World random!')
-        }, 30000);
+        fibonacci_series(10000)
+        res.send('Hello World fibonacci! ')
+        
     } else {
         res.send('Hello World!')
     }
---
+++ test.js
@@ -0,0 +1,24 @@
+var axios = require('axios');
+
+var config = {
+    method: 'get',
+    url: 'http://54.147.16.15:3000/',
+    headers: { }
+  };
+  axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+setInterval(() => {
+        axios(config)
+        .then(function (response) {
+          console.log(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+}, 31000)
